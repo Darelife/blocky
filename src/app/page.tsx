b54c7b79a7ea653f0454a1404bd7dd2cb3beb52a
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-01-18 15:03:35 +0530

    lander

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,9 @@ export default function Home() {
       )}
 
       <NavBar toggleTheme={toggleTheme} />
+      <div className="flex justify-center items-center min-h-screen mt-[-4em]">
+        <h1 className="md:text-9xl text-6xl font-mono font-black">BLOCKY</h1>
+      </div>
     </div>
   );
 }
