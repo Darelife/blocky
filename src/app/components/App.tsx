d8bf44929d65e963aabd485a8092351bc3ef53be
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-01-19 16:22:31 +0530

    idk whats going on

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -5,9 +5,9 @@ import DialogBox from './DialogBox';
 const App: React.FC = () => {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-    const handleAddButtonClick = () => {
-        setIsDialogOpen(true);
-    };
+    // const handleAddButtonClick = () => {
+    //     setIsDialogOpen(true);
+    // };
 
     const closeDialog = () => {
         setIsDialogOpen(false);
@@ -16,7 +16,7 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <h1>Subscription Manager</h1>
-            <AddButton onClick={handleAddButtonClick} />
+            <AddButton />
             <DialogBox isOpen={isDialogOpen} onClose={closeDialog} />
         </div>
     );
