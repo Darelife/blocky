dfbe2d431b0d09d29e64842e26d32a00f38d0aa7
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-01-19 18:42:10 +0530

    responsive

--- src/app/components/DashBoard.tsx
+++ src/app/components/DashBoard.tsx
@@ -4,7 +4,7 @@ import Subscriptions from "./Subscriptions";
 
 export default function DashBoard() {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
       {/* <NumSubs initialCount={4} /> */}
       <Left />
       <Subscriptions />
